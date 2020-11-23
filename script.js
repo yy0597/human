562c5e9bb77bb3d60a9c5d3d52e88fa91c9ef232
yy0597 <yy0597@naver.com>
2020-11-23 22:50:55 +0900

    human

---
+++ script.js
@@ -0,0 +1,70 @@
+const icon = document.querySelector('.icon');
+const on = document.querySelector('.fas.fa-video');
+const off = document.querySelector('.fas.fa-video-slash');
+const camera = document.querySelector('.camera');
+const name = document.querySelector('.name');
+const percent = document.querySelector('.percent');
+const description = document.querySelector('.description');
+
+const URL = './model/';
+
+let model, webcam, maxPredictions;
+
+icon.addEventListener('click', async () => {
+  on.classList.toggle('invisible');
+  off.classList.toggle('invisible');
+
+  if (on.classList.length === 2) {
+    await webcam.pause();
+  } else {
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+  }
+});
+
+async function init() {
+  const modelURL = URL + 'model.json';
+  const metadataURL = URL + 'metadata.json';
+
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  const flip = true;
+  webcam = new tmImage.Webcam(380, 380, flip);
+  await webcam.setup();
+
+  camera.appendChild(webcam.canvas);
+}
+
+async function loop() {
+  webcam.update();
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+init();
+
+async function predict() {
+  const prediction = await model.predict(webcam.canvas);
+
+  for (let i = 0; i < maxPredictions; i++) {
+    const className = prediction[i].className;
+    const probability = prediction[i].probability.toFixed(2) * 100;
+
+    const data = {
+      Me: 'Me에 대한 설명을 적어볼까요?',
+      None: 'None에 대한 설명을 적어볼까요?',
+    };
+
+    if (probability >= 75) {
+      if (name.innerHTML !== className) {
+        name.innerHTML = className;
+        description.innerHTML = data[className];
+      }
+
+      if (percent.innerHTML !== probability + '%') {
+        percent.innerHTML = probability + '%';
+      }
+    }
+  }
+}
